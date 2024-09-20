b7aeff2569aa9605b10ff61586c0a86dc9d6a2f2
hamidrm <hamidrmehrabian@gmail.com>
2024-09-20 17:01:51 +0200

    Clean up

--- methods/FedAvg.py
+++ methods/FedAvg.py
@@ -6,13 +6,14 @@ from core.FederatedLearningClass import *
 import random
 from utils.logger import *
 from utils.profiler import *
+from utils.common import Common
 
 class FedAvg(FederatedLearningClass):
 
     def __init__(self, args = ()):
         super().__init__()
         self.clients_epochs, self.num_of_rounds, self.datasets_weights, self.platform, extra_args = args
-        self.contributors_percent = int(common.Common.get_param_in_args(extra_args, "contributors_percent", 100))
+        self.contributors_percent = int(Common.get_param_in_args(extra_args, "contributors_percent", 100))
         self.num_of_nodes_contributor = 0
         self.round_num = 0
 
@@ -27,12 +28,6 @@ class FedAvg(FederatedLearningClass):
         for key in global_model.keys():
             torch_list_weights = torch.stack([clients_models[i][key].float() * fedavg_fraction[i] for i in range(len(clients_models))],0)
             global_model[key] = torch_list_weights.sum(0)
-        
-        StdD = self.calculate_sigma_l2_norm(clients_models, global_model)
-        profiler().save_variable("StdD", StdD, self.round_num)
-        logger.log_normal(f"===================================================")
-        logger.log_normal(f"= Std.Dev. : {StdD}")
-        logger.log_normal(f"===================================================")
         self.round_num += 1
 
 
@@ -73,31 +68,3 @@ class FedAvg(FederatedLearningClass):
             return True
         else:
             return False
-        
-    def calculate_sigma_l2_norm(self, local_models, global_model):
-        # Number of local models
-        N = len(local_models)
-        
-        # Initialize a dictionary to store the sum of squared deviations for each parameter
-        sum_squared_deviations = {}
-        
-        # Iterate over each parameter in the global model
-        for param_name, global_param in global_model.items():
-            # Initialize sum of squared deviations for this parameter
-            sum_squared_deviations[param_name] = torch.zeros_like(global_param)
-            
-            # Accumulate squared deviations from the global parameter for this parameter across all local models
-            for local_model in local_models:
-                local_param = local_model[param_name]
-                sum_squared_deviations[param_name] += (local_param - global_param) ** 2
-            
-            # Calculate the mean of squared deviations for this parameter
-            sum_squared_deviations[param_name] /= N
-        
-        # Calculate the standard deviation for each parameter
-        std_devs = {param_name: torch.sqrt(squared_deviation) for param_name, squared_deviation in sum_squared_deviations.items()}
-        
-        # Calculate the L2 norm of the standard deviations
-        l2_norm = torch.sqrt(sum(torch.sum(std_dev ** 2) for std_dev in std_devs.values()))
-        
-        return l2_norm.item()
